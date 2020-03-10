c6a17daf4781a1ad54971576fa80bd1b10065749
Velin Georgiev <velin.georgiev@gmail.com>
2020-03-10 23:57:08 +0000

    initial git added

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('sdfs');
+const fs = require('fs');
+const rimraf = require("rimraf");
+
+//console.log(process.argv);
+
+const resetCommit = 'b93282d74c23e79494b6d2a592b9eaffc5fdf9eb';
+const prRepo = 'https://github.com/plamber/office365-cli'; //process.argv[2];
+const prRepoBranch = 'skype-reports-activitycounts'; //process.argv[3];
+const dir = './repo';
+
+rimraf.sync(dir);
+fs.mkdirSync(dir);
+
+const git = require('simple-git')('./repo');
+
+git
+.clone(prRepo, './')
+.checkoutBranch('test1', `origin/${prRepoBranch}`)
+.log((err, log) => console.log(log.all[0]))
+.reset([resetCommit])
+.add('.')
+.status((err, status) => {
+    console.log(status);
+});
